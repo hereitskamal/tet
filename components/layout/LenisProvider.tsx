0285b78ddd6a8b7cbe5c4ddf07c53d14743437e0
Kamal <itskamalofficial@gmail.com>
2026-04-15 12:20:10 +0530

    new done

--- components/layout/LenisProvider.tsx
+++ components/layout/LenisProvider.tsx
@@ -13,12 +13,8 @@ export default function LenisProvider({ children }: LenisProviderProps) {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      smoothTouch: false,
       touchMultiplier: 2,
-      autoRaf: false, // We'll handle RAF manually for better control
+      autoRaf: false,
     })
 
     // Animation loop
